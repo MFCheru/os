b25e44771af15cf113005499fe5be3a3200b61a7
mbaigorria <martinbaigorria@gmail.com>
2015-09-22 01:37:53 -0300

    Clean code

--- pset1/simusched/sched_rr.cpp
+++ pset1/simusched/sched_rr.cpp
@@ -11,13 +11,18 @@ using namespace std;
 
 SchedRR::SchedRR(vector<int> argn) {
 	// Round robin recibe la cantidad de cores y sus cpu_quantum por parámetro
-	quantum = argn[1];
+	quantum = new int[argn.size()-1];
+
+	for (int i = 1; i < (int) argn.size(); i++) {
+		quantum[i-1] = argn[i];
+	}
+
 	cycles = new int[argn[0]];
-	fill_n(cycles, argn[0], quantum);
 }
 
 SchedRR::~SchedRR() {
 	delete[] cycles;
+	delete[] quantum;
 }
 
 
@@ -26,34 +31,34 @@ void SchedRR::load(int pid) {
 }
 
 void SchedRR::unblock(int pid) {
-
+	q.push(pid);
 }
 
 int SchedRR::tick(int cpu, const enum Motivo m) {
-	if (m == EXIT) {
+	if (m == EXIT || m == BLOCK) {
 		// Si el pid actual terminó, sigue el próximo.
 		if (q.empty()) return IDLE_TASK;
 		else {
 			int sig = q.front(); q.pop();
-			cycles[cpu] = quantum;
+			cycles[cpu] = quantum[cpu];
 			return sig;
 		}
 	} else {
 		if (current_pid(cpu) == IDLE_TASK && !q.empty()) {
 			int sig = q.front(); q.pop();
-			cycles[cpu] = quantum;
+			cycles[cpu] = quantum[cpu];
 			return sig;
 		} else {
 			cycles[cpu]--;
 
 			if (cycles[cpu] == 0) {
 				if (q.empty()) {
-					cycles[cpu] = quantum;
+					cycles[cpu] = quantum[cpu];
 					return current_pid(cpu);
 				} else {
 					int sig = q.front(); q.pop();
 					q.push(current_pid(cpu)); // re-add to queue
-					cycles[cpu] = quantum;
+					cycles[cpu] = quantum[cpu];
 					return sig;
 				}
 			} else {
