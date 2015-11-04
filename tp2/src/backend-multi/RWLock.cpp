dc6802e84848b30015c4ba02fe068f9bb78da9b4
CheruMF <cheru.mf@gmail.com>
2015-11-04 16:17:40 -0300

    Informe e implementación del RWLock.

--- tp2/src/backend-multi/RWLock.cpp
+++ tp2/src/backend-multi/RWLock.cpp
@@ -12,6 +12,7 @@ RWLock :: RWLock() {
 
 	pthread_mutex_init(&(this->lock_mutex), NULL);
 	pthread_mutex_init(&(this->lock_writer), NULL);
+	pthread_mutex_init(&(this->lock_reader), NULL);
 	pthread_cond_init (&(this->condition), NULL);
 	writer = false;
 	readers = 0;
@@ -23,7 +24,10 @@ void RWLock :: rlock() {
 
 	while (writer) 
 		pthread_cond_wait(&(this->condition), &(this->lock_mutex));
+
+	pthread_mutex_lock(&(this->lock_reader));
 	readers++;
+	pthread_mutex_unlock(&(this->lock_reader));
 	
 	pthread_mutex_unlock(&(this->lock_mutex));
 }
@@ -50,7 +54,10 @@ void RWLock :: runlock() {
 
 	pthread_mutex_lock(&(this->lock_mutex));
 	
+	pthread_mutex_lock(&(this->lock_reader));
 	readers--;
+	pthread_mutex_unlock(&(this->lock_reader));
+
 	if (readers == 0)
 		pthread_cond_broadcast(&(this->condition));
 	
--- tp2/src/backend-multi/RWLock.h
+++ tp2/src/backend-multi/RWLock.h
@@ -12,7 +12,8 @@ class RWLock {
 
     private:
 	    pthread_mutex_t lock_mutex;
-        pthread_mutex_t lock_writer;
+            pthread_mutex_t lock_writer;
+	    pthread_mutex_t lock_reader;
 	    pthread_cond_t condition;	
 	    bool writer;
 	    unsigned int readers;
